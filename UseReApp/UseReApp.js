0028648115d736e19eb00f653246e31de83b37cc
Venkata Muttana <ramanamuttana@gmail.com>
2025-05-17 12:06:07 +0200

    Update UseReApp.js

--- UseReApp/UseReApp.js
+++ UseReApp/UseReApp.js
@@ -1,1 +1,36 @@
 
+import "./App.css";
+import { useRef, useState } from "react";
+
+function UseRefApp() {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const credentials = {
+      password: passwordRef.current.value,
+      email: emailRef.current.value,
+    };
+    console.log(credentials);
+    console.log("Submitted!");
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <p>
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" ref={emailRef} />
+      </p>
+      <p>
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" ref={passwordRef} />
+      </p>
+      <p className="actions">
+        <button>Login</button>
+      </p>
+    </form>
+  );
+}
+
+export default UseRefApp;
